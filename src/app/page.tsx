ed995eadbd7707d995d6fa77af9d9a4df7173f49
Seoyun Kim <seoyunkim@Seoyunui-MacBookPro.local>
2024-12-18 19:20:52 +0900

    style:setting css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,219 @@
 import Image from "next/image";
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className="w-full h-full py-[60px] flex flex-col justify-center">
+      <div className="md:w-[608px] pb-[60px] mx-auto my-auto bg-white rounded-[30px] flex flex-col items-center justify-center">
+        <main className="flex flex-col items-center justify-center">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/cover.svg"
+            alt="cover image"
+            width={608}
+            height={142}
+            priority
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/avatar.svg"
+            alt="profile avatar"
+            width={180}
+            height={180}
+            priority
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <h1 className="text-4xl font-bold mt-[18px]">Seoyun Kim</h1>
+          <p className="text-md text-center font-semibold text-neutral-600 mt-[4px] hidden sm:block">
+            Feel free to explore my GitHub, Notion, Velog, and LinkedIn.
+            <br />
+            You can find more information at the link below!
+          </p>
+          <div className="flex justify-center gap-[10px] mt-[16px]">
+            <a
+              href="https://github.com/kimseoyun98"
+              target="_blank"
+              rel="github link"
+            >
+              <Image
+                src="/github.svg"
+                alt="github image"
+                width={32}
+                height={32}
+                className="transition-transform duration-200 ease-in-out transform hover:scale-110"
+              />
+            </a>
+            <a
+              href="https://www.notion.so/marisportfolio/DesignedBy-Mari-13d32ee427364e50a1ffbc50e7f283ba"
+              target="_blank"
+              rel="notion link"
+            >
+              <Image
+                src="/notion.svg"
+                alt="notion image"
+                width={31.52}
+                height={31.6}
+                className="transition-transform duration-200 ease-in-out transform hover:scale-110"
+              />
+            </a>
+            <a
+              href="www.linkedin.com/in/seoyunmari"
+              target="_blank"
+              rel="linkedIn link"
+            >
+              <Image
+                src="/linkedin.svg"
+                alt="linkedin image"
+                width={31.52}
+                height={31.6}
+                className="transition-transform duration-200 ease-in-out transform hover:scale-110"
+              />
+            </a>
+            <a
+              href="https://velog.io/@gimmari/posts"
+              target="_blank"
+              rel="velog link"
+            >
+              <Image
+                src="/velog.svg"
+                alt="velog image"
+                width={31.52}
+                height={31.6}
+                className="transition-transform duration-200 ease-in-out transform hover:scale-110"
+              />
+            </a>
+          </div>
+          <div className="flex flex-col w-full mx-auto items-center justify-center gap-[12px] mt-[30px]">
+            <div className="flex w-[316px] sm:w-[400px] py-[9px] pl-[24px] pr-[9px] border border-neutral-300 rounded-[74.82px] justify-between shadow-short-default hover:bg-neutral-800 hover:text-neutral-100 hover:shadow-none transition-colors duration-150 ease-in-out">
+              <p className="flex items-center text-left text-md font-bold">
+                👩🏻‍💻 Visit my FE portfolio
+              </p>
+              <a
+                className="group inline-flex items-center justify-center"
+                href="https://fe developer portfolio link"
+                target="_blank"
+                rel="fe developer portfolio link"
+              >
+                <svg
+                  className="transition-colors duration-300 ease-in-out"
+                  width="48"
+                  height="48"
+                  viewBox="0 0 48 48"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="24"
+                    cy="24"
+                    r="24"
+                    className="transition-colors duration-300 ease-in-out fill-neutral-300 group-hover:fill-neutral-100"
+                  />
+                  <path
+                    d="M16.6257 30L30 16.592"
+                    className="transition-colors duration-300 ease-in-out stroke-neutral-500 group-hover:stroke-neutral-800"
+                    strokeWidth="1.87057"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M30 29.2324V16.5913H17.3906"
+                    className="transition-colors duration-300 ease-in-out stroke-neutral-500 group-hover:stroke-neutral-800"
+                    strokeWidth="1.87057"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="flex w-[316px] sm:w-[400px] py-[9px] pl-[24px] pr-[9px] border border-neutral-300 rounded-[74.82px] justify-between shadow-short-default hover:bg-neutral-800 hover:text-neutral-100 hover:shadow-none transition-colors duration-150 ease-in-out">
+              <p className="flex items-center text-left text-md font-bold">
+                📝 Visit my Design portfolio
+              </p>
+              <a
+                className="group inline-flex items-center justify-center"
+                href="https://disign portfolio link"
+                target="_blank"
+                rel="disign portfolio link"
+              >
+                <svg
+                  className="transition-colors duration-300 ease-in-out"
+                  width="48"
+                  height="48"
+                  viewBox="0 0 48 48"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="24"
+                    cy="24"
+                    r="24"
+                    className="transition-colors duration-300 ease-in-out fill-neutral-300 group-hover:fill-neutral-100"
+                  />
+                  <path
+                    d="M16.6257 30L30 16.592"
+                    className="transition-colors duration-300 ease-in-out stroke-neutral-500 group-hover:stroke-neutral-800"
+                    strokeWidth="1.87057"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M30 29.2324V16.5913H17.3906"
+                    className="transition-colors duration-300 ease-in-out stroke-neutral-500 group-hover:stroke-neutral-800"
+                    strokeWidth="1.87057"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </a>
+            </div>
+            <div className="flex w-[316px] sm:w-[400px] py-[9px] pl-[24px] pr-[9px] border border-neutral-300 rounded-[74.82px] justify-between shadow-short-default hover:bg-neutral-800 hover:text-neutral-100 hover:shadow-none transition-colors duration-150 ease-in-out">
+              <p className="flex items-center text-left text-md font-bold">
+                🎙 About Me !
+              </p>
+              <a
+                className="group inline-flex items-center justify-center"
+                href="https://about me link"
+                target="_blank"
+                rel="about me link"
+              >
+                <svg
+                  className="transition-colors duration-300 ease-in-out"
+                  width="48"
+                  height="48"
+                  viewBox="0 0 48 48"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="24"
+                    cy="24"
+                    r="24"
+                    className="transition-colors duration-300 ease-in-out fill-neutral-300 group-hover:fill-neutral-100"
+                  />
+                  <path
+                    d="M16.6257 30L30 16.592"
+                    className="transition-colors duration-300 ease-in-out stroke-neutral-500 group-hover:stroke-neutral-800"
+                    strokeWidth="1.87057"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                  <path
+                    d="M30 29.2324V16.5913H17.3906"
+                    className="transition-colors duration-300 ease-in-out stroke-neutral-500 group-hover:stroke-neutral-800"
+                    strokeWidth="1.87057"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </main>
+      </div>
+      <div className="w-full mx-auto my-auto">
+        <p className="text-center">
+          Designed by Mari.
+          <br />© 2024 Mari. All rights reserved.
+        </p>
+      </div>
     </div>
   );
-}
+};
+
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
+import safeArea from "tailwindcss-safe-area";
+import tailwindAnimate from "tailwindcss-animate";
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +12,37 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        neutral: {
+          100: "#ffffff",
+          200: "#f7f7f7",
+          300: "#eff2f6",
+          400: "#d9dfe9",
+          500: "#a0abbd",
+          600: "#5c6779",
+          700: "#4f5a6e",
+          800: "#030b1a",
+        },
+      },
+      fontSize: {
+        sm: "0.875rem",
+        md: "1rem", // base
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.375rem",
+        "3xl": "1.5rem",
+        "4xl": "1.75rem",
+        "5xl": "1.875rem",
+        "6xl": "2.125rem",
+        "7xl": "2.25rem",
+        "8xl": "3.125rem",
+      },
+      boxShadow: {
+        "short-default": "0px 5px 12px 0px rgba(8,15,52,0.05)",
+        "short-hover": "0px 18px 24px 0px rgba(8,15,52,0.08)",
+        long: "0px 14px 42px 0px rgba(8,15,52,0.06)",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [safeArea, tailwindAnimate, tailwindScrollbarHide],
+};
+export default config;
