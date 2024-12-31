fadf1284bc5ce5c66b130b99ce1ef2c1745e373b
Seoyun Kim <seoyunkim@Seoyunui-MacBookPro.local>
2024-12-31 20:26:13 +0900

    final commit

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/web-portfolio",
   assetPrefix: "/web-portfolio",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
