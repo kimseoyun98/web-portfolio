ec13f534cce7df0602a4d215caa2a3de0f87868a
Seoyun Kim <seoyunkim@Seoyunui-MacBookPro.local>
2024-12-31 20:30:47 +0900

    del export

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/web-portfolio",
-  assetPrefix: "/web-portfolio",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
